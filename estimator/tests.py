8f34c9dc9f27704e93eefe975a5eb7d6d2e41186
Isaac Barrezueta <barrezuetai@allegheny.edu>
2020-03-04 17:37:19 -0500

    starting test cases

--- estimator/tests.py
+++ estimator/tests.py
@@ -1,3 +1,10 @@
-# from django.test import TestCase
+from django.test import TestCase
+
 
 # Create your tests here.
+class PDFScanTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_pdf_scans(self):
+        self.assertEqual(1, 2)
