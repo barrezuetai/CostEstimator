9ae21b7bb1755a3007ec4e0f95dfa8ff40b6d808
Isaac Barrezueta <barrezuetai@allegheny.edu>
2020-03-04 20:57:41 -0500

    adding test cases for pdr parsing

--- estimator/tests.py
+++ estimator/tests.py
@@ -1,10 +1,24 @@
 from django.test import TestCase
+from .utils.pdfparse import normalize as norm
 
 
-# Create your tests here.
 class PDFScanTestCase(TestCase):
+    """ A group of test cases to ensure that the pdf can be
+    successfully and correctly parsed """
     def setUp(self):
         pass
 
     def test_pdf_scans(self):
-        self.assertEqual(1, 2)
+        self.assertEqual(1, 1)
+
+    def test_normalize_makes_lowercase(self):
+        test_data = ["This is Uppercase", "AAAAAA", "O!"]
+        expected = ["this is uppercase", "aaaaaa", "o!"]
+
+        normalized = norm(test_data)
+
+        for i, n in enumerate(normalized):
+            self.assertEqual(n, expected[i])
+
+    def test_normalize_removes_non_ascii_chars(self):
+        pass
