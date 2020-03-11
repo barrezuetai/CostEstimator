d913bd814bac28c8dc12aa718ae8c3254f02bb18
Isaac Barrezueta <barrezuetai@allegheny.edu>
2020-03-11 00:14:17 -0400

    Added rudimentary price cost ratio module for form

---
+++ estimator/utils/estimate.py
@@ -0,0 +1,9 @@
+"""
+This module consists of the functions needed
+in order to find a hospital's pcr
+"""
+
+
+def pcr():
+    """ Calculates and returns the pcr given proper parameters """
+    return 2  # just hardcoding a value for testing rn
