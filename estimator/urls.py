59162e11db4091d98c5106ae1ea843e57e5157f5
Isaac Barrezueta <barrezuetai@allegheny.edu>
2020-04-04 15:39:30 -0400

    Added basic functionality to form and connected it to detail view

--- estimator/urls.py
+++ estimator/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from estimator import views
+from estimator.views import HospitalDetailView
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("hospital/", views.create_hospital_page, name="create_hospital"),
     path("hospital/<int:pk>/", HospitalDetailView.as_view(),
          name="hospital_detail"),
 ]
--- estimator/views.py
+++ estimator/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from estimator.forms import HospitalForm
 from django.views.generic import DetailView
 from estimator.models import Hospital
 
@@ -14,3 +17,21 @@ def home(request):
             'title': 'Price Estimator',
         }
     )
+
+
+def create_hospital_page(request):
+    if request.method == 'POST':
+        formset = HospitalForm(request.POST, request.FILES)
+        if formset.is_valid():
+            formset.save()
+            id = formset.instance.id
+            return redirect(reverse("hospital_detail", args=(id,)))
+    else:
+        formset = HospitalForm()
+    return render(
+        request,
+        'estimator/manage_hospital.html',
+        {'formset': formset}
+    )
+
+
